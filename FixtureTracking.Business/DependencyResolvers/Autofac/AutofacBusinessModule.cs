60dad49ffa0e0df8339b45e0be9432887a1d7cd6
MustafaCEVIK <se.mustafacevik@gmail.com>
2020-12-08 23:30:00 +0300

    Added autofac business module class

---
+++ FixtureTracking.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using FixtureTracking.Business.Abstract;
+using FixtureTracking.Business.Concrete;
+using FixtureTracking.DataAccess.Abstract;
+using FixtureTracking.DataAccess.Concrete.EntityFramework;
+
+namespace FixtureTracking.Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CompanyManager>().As<ICompanyService>();
+            builder.RegisterType<EfCompanyDal>().As<ICompanyDal>();
+        }
+    }
+}
