de65ba0f1e8635550eb303b254d2079f5762cb4e
MustafaCEVIK <se.mustafacevik@gmail.com>
2020-12-28 01:32:37 +0300

    Refactored mock class, built entity repository mock class

---
+++ FixtureTracking.Tests/Mocks/MockEntityRepository.cs
@@ -0,0 +1,66 @@
+﻿using FixtureTracking.Core.DataAccess;
+using FixtureTracking.Core.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace FixtureTracking.Tests.Mocks
+{
+    public class MockEntityRepository<TEntity, TRepository> : Mock<TRepository>
+        where TEntity : class, IEntity, new()
+        where TRepository : class, IEntityRepository<TEntity>
+    {
+        public MockEntityRepository<TEntity, TRepository> MockGet(TEntity result)
+        {
+            Setup(x => x.Get(It.IsAny<Expression<Func<TEntity, bool>>>()))
+               .Returns(result);
+
+            return this;
+        }
+
+        public MockEntityRepository<TEntity, TRepository> MockGetList(List<TEntity> results)
+        {
+            Setup(x => x.GetList(It.IsAny<Expression<Func<TEntity, bool>>>()))
+               .Returns(results);
+
+            return this;
+        }
+
+        public MockEntityRepository<TEntity, TRepository> MockAdd(TEntity result)
+        {
+            Setup(x => x.Add(It.IsAny<TEntity>()))
+               .Returns(result);
+
+            return this;
+        }
+
+        public MockEntityRepository<TEntity, TRepository> MockUpdate()
+        {
+            Setup(x => x.Update(It.IsAny<TEntity>()));
+
+            return this;
+        }
+
+        public MockEntityRepository<TEntity, TRepository> VerifyUpdate(Times times)
+        {
+            Verify(x => x.Update(It.IsAny<TEntity>()), times);
+
+            return this;
+        }
+
+        public MockEntityRepository<TEntity, TRepository> MockDelete()
+        {
+            Setup(x => x.Delete(It.IsAny<TEntity>()));
+
+            return this;
+        }
+
+        public MockEntityRepository<TEntity, TRepository> VerifyDelete(Times times)
+        {
+            Verify(x => x.Delete(It.IsAny<TEntity>()), times);
+
+            return this;
+        }
+    }
+}
