78958b9948f62eadb4766a9092572042e7a9a359
MustafaCEVIK <se.mustafacevik@gmail.com>
2021-01-04 22:38:18 +0300

    Added token utility objects

---
+++ FixtureTracking.Core/Utilities/Security/Tokens/ITokenHelper.cs
@@ -0,0 +1,9 @@
+﻿using FixtureTracking.Core.Entities.Concrete;
+
+namespace FixtureTracking.Core.Utilities.Security.Tokens
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, string[] claimNames);
+    }
+}
---
+++ FixtureTracking.Core/Utilities/Security/Tokens/Jwt/JwtHelper.cs
@@ -0,0 +1,23 @@
+﻿using FixtureTracking.Core.Entities.Concrete;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace FixtureTracking.Core.Utilities.Security.Tokens.Jwt
+{
+    public class JwtHelper : ITokenHelper
+    {
+        public IConfiguration Configuration { get; }
+        private readonly TokenOptions tokenOptions;
+
+        public JwtHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            tokenOptions = Configuration.GetSection("Tokenoptions").Get<TokenOptions>();
+        }
+
+        public AccessToken CreateToken(User user, string[] claimNames)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
