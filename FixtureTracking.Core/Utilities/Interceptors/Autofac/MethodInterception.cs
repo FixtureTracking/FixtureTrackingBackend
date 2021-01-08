2c47a5ef2a5d6d4a3c7dfe1798dd8b804f28571d
MustafaCEVIK <se.mustafacevik@gmail.com>
2021-01-08 18:26:27 +0300

    Added method intercepiton

---
+++ FixtureTracking.Core/Utilities/Interceptors/Autofac/MethodInterception.cs
@@ -0,0 +1,37 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace FixtureTracking.Core.Utilities.Interceptors.Autofac
+{
+    public class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation) { }
+        protected virtual void OnAfter(IInvocation invocation) { }
+        protected virtual void OnException(IInvocation invocation) { }
+        protected virtual void OnSuccess(IInvocation invocation) { }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception e)
+            {
+                isSuccess = false;
+                OnException(invocation);
+                throw e;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+            }
+            OnAfter(invocation);
+        }
+    }
+}
---
+++ FixtureTracking.Core/Utilities/Interceptors/Autofac/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace FixtureTracking.Core.Utilities.Interceptors.Autofac
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; }
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+        }
+    }
+}
