4cdddbe4f5a2e51e030bd36a5264bd585bc38b1b
MustafaCEVIK <se.mustafacevik@gmail.com>
2021-01-08 22:06:28 +0300

    Added validation aspect

---
+++ FixtureTracking.Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -0,0 +1,33 @@
+﻿using Castle.DynamicProxy;
+using FixtureTracking.Core.CrossCuttingConcerns.Validation.FluentValidation;
+using FixtureTracking.Core.Utilities.Interceptors.Autofac;
+using FixtureTracking.Core.Utilities.Messages;
+using FluentValidation;
+using System;
+using System.Linq;
+
+namespace FixtureTracking.Core.Aspects.Autofac.Validation
+{
+    public class ValidationAspect : MethodInterception
+    {
+        private readonly Type validatorType;
+        public ValidationAspect(Type validatorType)
+        {
+            if (!typeof(IValidator).IsAssignableFrom(validatorType))
+                throw new Exception(AspectMessages.WrongValidationType);
+
+            this.validatorType = validatorType;
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var validator = (IValidator)Activator.CreateInstance(validatorType);
+            var entityType = validatorType.BaseType.GetGenericArguments()[0];
+            var entities = invocation.Arguments.Where(t => t.GetType() == entityType);
+            foreach (var entity in entities)
+            {
+                ValidationTool.Validate(validator, entity);
+            }
+        }
+    }
+}
---
+++ FixtureTracking.Core/Utilities/Messages/AspectMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace FixtureTracking.Core.Utilities.Messages
+{
+    public static class AspectMessages
+    {
+        public static string WrongValidationType = "Wrong Validation Type";
+    }
+}
