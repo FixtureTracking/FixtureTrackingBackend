2261d65700d968c17a3db6de3543d8850d0fc9cb
MustafaCEVIK <se.mustafacevik@gmail.com>
2021-01-10 01:19:33 +0300

    Added cache remove aspect

---
+++ FixtureTracking.Core/Aspects/Autofac/Caching/CacheRemoveAspect.cs
@@ -0,0 +1,26 @@
+﻿using Castle.DynamicProxy;
+using FixtureTracking.Core.CrossCuttingConcerns.Caching;
+using FixtureTracking.Core.Utilities.Interceptors.Autofac;
+using FixtureTracking.Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FixtureTracking.Core.Aspects.Autofac.Caching
+{
+    public class CacheRemoveAspect : MethodInterception
+    {
+        private readonly string pattern;
+        private readonly ICacheManager cacheManager;
+
+        public CacheRemoveAspect(string pattern)
+        {
+            this.pattern = pattern;
+            cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+        }
+
+        protected override void OnSuccess(IInvocation invocation)
+        {
+            cacheManager.RemovePattern(pattern);
+        }
+
+    }
+}
