3291f7d6bc5f6f33ef54c661dcc328cc19b48a7b
MustafaCEVIK <se.mustafacevik@gmail.com>
2021-01-09 21:52:19 +0300

    Added  service collection extensions

---
+++ FixtureTracking.Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FixtureTracking.Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FixtureTracking.Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDependencyResolvers(this IServiceCollection services, ICoreModule[] modules)
+        {
+            foreach (var module in modules)
+            {
+                module.Load(services);
+            }
+            return ServiceTool.Create(services);
+        }
+    }
+}
