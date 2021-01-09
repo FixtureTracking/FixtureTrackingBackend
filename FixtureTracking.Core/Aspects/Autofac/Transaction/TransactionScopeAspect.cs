f5d75aa243482dee6a947c6b8054672e6c18888a
MustafaCEVIK <se.mustafacevik@gmail.com>
2021-01-09 18:02:58 +0300

    Added transaction scope aspect

---
+++ FixtureTracking.Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,26 @@
+﻿using Castle.DynamicProxy;
+using FixtureTracking.Core.Utilities.Interceptors.Autofac;
+using System;
+using System.Transactions;
+
+namespace FixtureTracking.Core.Aspects.Autofac.Transaction
+{
+    public class TransactionScopeAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using var transactionScope = new TransactionScope();
+
+            try
+            {
+                invocation.Proceed();
+                transactionScope.Complete();
+            }
+            catch (Exception e)
+            {
+                transactionScope.Dispose();
+                throw e;
+            }
+        }
+    }
+}
