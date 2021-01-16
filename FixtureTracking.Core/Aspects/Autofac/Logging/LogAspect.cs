0ba26b726d84c3b1bf3a53d18008ee0f2293a017
MustafaCEVIK <se.mustafacevik@gmail.com>
2021-01-16 17:56:09 +0300

    Added log aspect

---
+++ FixtureTracking.Core/Aspects/Autofac/Logging/LogAspect.cs
@@ -0,0 +1,51 @@
+﻿using Castle.DynamicProxy;
+using FixtureTracking.Core.CrossCuttingConcerns.Logging;
+using FixtureTracking.Core.CrossCuttingConcerns.Logging.NLog;
+using FixtureTracking.Core.Utilities.Interceptors.Autofac;
+using FixtureTracking.Core.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+
+namespace FixtureTracking.Core.Aspects.Autofac.Logging
+{
+    public class LogAspect : MethodInterception
+    {
+        private readonly LoggerServiceBase loggerServiceBase;
+
+        public LogAspect(Type loggerService)
+        {
+            if (loggerService.BaseType != typeof(LoggerServiceBase))
+                throw new Exception(AspectMessages.WrongLoggerType);
+
+            loggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(loggerService);
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            loggerServiceBase.Info(GetLogDetail(invocation));
+        }
+
+        private LogDetail GetLogDetail(IInvocation invocation)
+        {
+            var logPrameters = new List<LogParameter>();
+
+            for (int i = 0; i < invocation.Arguments.Length; i++)
+            {
+                logPrameters.Add(new LogParameter()
+                {
+                    Name = invocation.GetConcreteMethod().GetParameters()[i].Name,
+                    Value = invocation.Arguments[i],
+                    Type = invocation.Arguments[i].GetType().Name
+                });
+            }
+
+            var logDetail = new LogDetail()
+            {
+                MethodName = $"{invocation.Method.ReflectedType.Name}.{invocation.Method.Name}",
+                LogParameters = logPrameters
+            };
+
+            return logDetail;
+        }
+    }
+}
