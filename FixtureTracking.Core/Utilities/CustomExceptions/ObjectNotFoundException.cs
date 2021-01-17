4e288db54c65e690420cb06bdedd096543a61709
MustafaCEVIK <se.mustafacevik@gmail.com>
2021-01-18 01:02:02 +0300

    Added object not found exception

---
+++ FixtureTracking.Core/Utilities/CustomExceptions/ObjectNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using FixtureTracking.Core.Utilities.Messages;
+using System;
+
+namespace FixtureTracking.Core.Utilities.CustomExceptions
+{
+    public class ObjectNotFoundException : Exception
+    {
+        public ObjectNotFoundException(string objectName) : base(ExceptionMessages.ObjectNotFound(objectName)) { }
+    }
+}
---
+++ FixtureTracking.Core/Utilities/Messages/ExceptionMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace FixtureTracking.Core.Utilities.Messages
+{
+    public static class ExceptionMessages
+    {
+        public static string ObjectNotFound(string objectName) => $"{objectName} not found";
+    }
+}
