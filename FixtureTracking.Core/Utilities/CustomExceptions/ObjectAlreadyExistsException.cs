918c703eacaf39728c4d82a5da0143a0a51c64dd
MustafaCEVIK <se.mustafacevik@gmail.com>
2021-01-18 17:19:03 +0300

    Added object already exists exception

---
+++ FixtureTracking.Core/Utilities/CustomExceptions/ObjectAlreadyExistsException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FixtureTracking.Core.Utilities.CustomExceptions
+{
+    public class ObjectAlreadyExistsException : Exception
+    {
+        public ObjectAlreadyExistsException(string message) : base(message) { }
+    }
+}
