85d40e1d627f20cb414436f71d7e4f86a296237b
MustafaCEVIK <se.mustafacevik@gmail.com>
2021-01-19 01:32:23 +0300

    Added exception middleware

---
+++ FixtureTracking.Core/Utilities/Middlewares/Exception/ErrorDetail.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace FixtureTracking.Core.Utilities.Middlewares.Exception
+{
+    public class ErrorDetail
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string ContentType { get; private set; }
+
+        public ErrorDetail()
+        {
+            ContentType = "application/json";
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(new { statusCode = StatusCode, message = Message });
+        }
+    }
+}
---
+++ FixtureTracking.Core/Utilities/Middlewares/Exception/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using FixtureTracking.Core.Utilities.CustomExceptions;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace FixtureTracking.Core.Utilities.Middlewares.Exception
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await next(httpContext);
+            }
+            catch (System.Exception e)
+            {
+                await HandleExceptionAsync(httpContext, e);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext httpContext, System.Exception e)
+        {
+            ErrorDetail errorDetail = new ErrorDetail();
+
+            if (e is HttpStatusException httpStatusException)
+            {
+                errorDetail.StatusCode = httpStatusException.HttpStatusCode;
+                errorDetail.Message = httpStatusException.Message;
+            }
+            else
+            {
+                errorDetail.StatusCode = 500;
+                errorDetail.Message = "Internal server error";
+            }
+
+            httpContext.Response.ContentType = errorDetail.ContentType;
+            httpContext.Response.StatusCode = errorDetail.StatusCode;
+            return httpContext.Response.WriteAsync(errorDetail.ToString());
+        }
+    }
+}
